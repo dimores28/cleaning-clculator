31dc11ace572ccc6941869448f5d585801146830
Dmytro <dmytroivanovichn@gmail.com>
2023-06-04 22:48:09 +0300

    calculator

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -16,6 +16,7 @@ $('.room').on('click', function() {
    roomPrice = price;
 });
 
+
 $('.service').on('click', function() {
    $(this).toggleClass('_select');
    let sum = 0;
@@ -30,20 +31,43 @@ $('.service').on('click', function() {
 });
 
 $('.calculator__btn-next').on('click', function() {
-
    if(step < 3) {
       step++;
-      $('.calculator__steps span').text(step);
+   }
+
+   stepControl();
+});
+
+$('.calculator__btn-prev').on('click', function() {
+   if(step >= 2) {
+      step--;
+   }
+   
+   stepControl();
+});
+
+
+function stepControl() {
+   $('.calculator__steps span').text(step);
+
+   if( step ===1 ) {
+      $('.calculator__square').show();
+      $('.calculator__form').hide();
+      $('.calculator__services').hide();
+      $('.calculator__btn-prev').hide();
    }
 
    if(step === 2) {
       $('.calculator__square').hide();
+      $('.calculator__form').hide();
+      $('.calculator__btn-prev').show();
+      $('.calculator__btn-next').show();
       $('.calculator__services').show();
    }
 
    if(step === 3) {
       $('.calculator__services').hide();
-      $(this).hide();
+      $('.calculator__btn-next').hide();
       $('.calculator__form').show();
    }
-});
+}
