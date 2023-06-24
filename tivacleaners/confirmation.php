00651d03bdbda8915a5edee1260cbe7c0828240d
Dmytro <dmytroivanovichn@gmail.com>
2023-06-24 15:59:52 +0300

    send new version site to server

---
+++ tivacleaners/confirmation.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Template Name: Confirmation
+*/
+?>
+
+<?php get_header();?>
+<section class="confirmation">
+   <div class="confirmation__container">
+      <h1 class="confirmation__heading">Thank you order!</h1>
+      <h2 class="confirmation__sub-heading">
+         Your application has been accepted, we will contact you shortly.
+      </h2>
+   </div>
+</section>
+<?php get_footer();?>
