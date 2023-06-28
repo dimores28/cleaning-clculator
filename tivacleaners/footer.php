4b792375683901757d61df9b85d912d5e522f1a9
maksym-krasovskyi <maxkrasovskyi10@gmail.com>
2023-06-28 23:28:08 +0300

    added custom fields for contacts

--- tivacleaners/footer.php
+++ tivacleaners/footer.php
@@ -1,4 +1,5 @@
 	</main>
+	<?php $contacts = getContacts(); ?>
 	<footer class="footer" id="contacts" data-watch="navigator">
 			<div class="footer__container">
 				<div class="footer__logo">
@@ -13,12 +14,12 @@
 						<h3 class="footer__heading">CONTACT US</h3>
 						<ul class="contacts">
 							<li class="contacts__item">
-								<a href="https://goo.gl/maps/g1emVUuC1TKwgNTZ7" class="contacts__link" target="_blank">
+								<a href="<?php echo $contacts["address_link"] ?>" class="contacts__link" target="_blank">
 									<svg class="contacts__icon" xmlns="http://www.w3.org/2000/svg" id="Filled" viewBox="0 0 24 24" width="512" height="512">
 										<path d="M12,.042a9.992,9.992,0,0,0-9.981,9.98c0,2.57,1.99,6.592,5.915,11.954a5.034,5.034,0,0,0,8.132,0c3.925-5.362,5.915-9.384,5.915-11.954A9.992,9.992,0,0,0,12,.042ZM12,14a4,4,0,1,1,4-4A4,4,0,0,1,12,14Z" />
 									</svg>
 
-									<span> Calgary. 1515-12st SW </span>
+									<span><?php echo $contacts["address"] ?></span>
 								</a>
 							</li>
 							<li class="contacts__item">
@@ -26,11 +27,11 @@
 									<path d="M24,6.24c0,7.64-10.13,17.76-17.76,17.76-1.67,0-3.23-.63-4.38-1.78l-1-1.15c-1.16-1.16-1.16-3.12,.05-4.33,.03-.03,2.44-1.88,2.44-1.88,1.2-1.14,3.09-1.14,4.28,0l1.46,1.17c3.2-1.36,5.47-3.64,6.93-6.95l-1.16-1.46c-1.15-1.19-1.15-3.09,0-4.28,0,0,1.85-2.41,1.88-2.44,1.21-1.21,3.17-1.21,4.38,0l1.05,.91c1.2,1.19,1.83,2.75,1.83,4.42Z" />
 								</svg>
 
-								<a href="tel:+18252887200" class="contacts__link">
-									<span>825-288-7200</span>
+								<a href="tel:<?php echo $contacts["primary_phone"] ?>" class="contacts__link">
+									<span><?php echo $contacts["primary_phone"] ?></span>
 								</a>
-								<a href="tel:+18252887202" class="contacts__link">
-									<span class="margin-left">825-288-7202</span>
+								<a href="tel:<?php echo $contacts["secondary_phone"] ?>" class="contacts__link">
+									<span class="margin-left"><?php echo $contacts["secondary_phone"] ?></span>
 								</a>
 							</li>
 						</ul>
@@ -47,12 +48,12 @@
 								<span>24/7</span>
 							</li>
 							<li class="more-info__item">
-								<a href="mailto:info@tivacleaners.com" class="more-info__link">
+								<a href="mailto:<?php echo $contacts["email"] ?>" class="more-info__link">
 									<svg class="more-info__icon" xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24" width="512" height="512">
 										<path d="M19,24h-6.66C5.87,24,.46,19.1,.03,12.85-.21,9.38,1.06,5.97,3.51,3.52,5.97,1.06,9.38-.21,12.85,.03c6.25,.43,11.15,5.84,11.15,12.31v5.82c0,3.44-2.06,5.85-5,5.85Z" />
 									</svg>
 
-									<span>info@tivacleaners.com</span>
+									<span><?php echo $contacts["email"] ?></span>
 								</a>
 							</li>
 						</ul>
@@ -61,7 +62,7 @@
 						<h3 class="footer__heading">FOLLOW US</h3>
 						<ul class="fol-us">
 							<li class="fol-us__item">
-								<a href="https://instagram.com/tiva.cleaners?igshid=Y2I2MzMwZWM3ZA" class="fol-us__link">
+								<a href="<?php echo $contacts["instagram"] ?>" class="fol-us__link">
 									<svg class="fol-us__icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 24 24" style="enable-background: new 0 0 24 24" xml:space="preserve" width="512" height="512">
 										<g>
 											<path d="M12,2.162c3.204,0,3.584,0.012,4.849,0.07c1.308,0.06,2.655,0.358,3.608,1.311c0.962,0.962,1.251,2.296,1.311,3.608   c0.058,1.265,0.07,1.645,0.07,4.849c0,3.204-0.012,3.584-0.07,4.849c-0.059,1.301-0.364,2.661-1.311,3.608   c-0.962,0.962-2.295,1.251-3.608,1.311c-1.265,0.058-1.645,0.07-4.849,0.07s-3.584-0.012-4.849-0.07   c-1.291-0.059-2.669-0.371-3.608-1.311c-0.957-0.957-1.251-2.304-1.311-3.608c-0.058-1.265-0.07-1.645-0.07-4.849   c0-3.204,0.012-3.584,0.07-4.849c0.059-1.296,0.367-2.664,1.311-3.608c0.96-0.96,2.299-1.251,3.608-1.311   C8.416,2.174,8.796,2.162,12,2.162 M12,0C8.741,0,8.332,0.014,7.052,0.072C5.197,0.157,3.355,0.673,2.014,2.014   C0.668,3.36,0.157,5.198,0.072,7.052C0.014,8.332,0,8.741,0,12c0,3.259,0.014,3.668,0.072,4.948c0.085,1.853,0.603,3.7,1.942,5.038   c1.345,1.345,3.186,1.857,5.038,1.942C8.332,23.986,8.741,24,12,24c3.259,0,3.668-0.014,4.948-0.072   c1.854-0.085,3.698-0.602,5.038-1.942c1.347-1.347,1.857-3.184,1.942-5.038C23.986,15.668,24,15.259,24,12   c0-3.259-0.014-3.668-0.072-4.948c-0.085-1.855-0.602-3.698-1.942-5.038c-1.343-1.343-3.189-1.858-5.038-1.942   C15.668,0.014,15.259,0,12,0z" />
